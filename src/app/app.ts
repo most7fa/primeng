a3431f1195bf549d99ff4d6a7d251cf43f5b27c2
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-21 15:32:59 +0300

    spin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { Check } from '@primeicons/angular/check';
-import { Times } from '@primeicons/angular/times';
-import { Search } from '@primeicons/angular/search';
-import { User } from '@primeicons/angular/user';
+import { Spinner } from '@primeicons/angular/spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Check, Times, Search, User],
+  imports: [Spinner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
