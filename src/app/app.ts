9de0ff9ba3694898b2af739ccad2520fd2d3a83f
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-21 14:46:55 +0300

    size

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+import { Check } from '@primeicons/angular/check';
+import { Times } from '@primeicons/angular/times';
+import { Search } from '@primeicons/angular/search';
+import { User } from '@primeicons/angular/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  standalone: true,
+  imports: [Check, Times, Search, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('primeng');
-}
+export class App {}
