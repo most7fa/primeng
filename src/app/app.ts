48e919279a2a5116fb2cb8adab2704bce7c8830d
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-21 15:37:46 +0300

    Programmatic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { Spinner } from '@primeicons/angular/spinner';
+import { Component, signal } from '@angular/core';
+import { PIcon } from '@primeicons/angular/p-icon';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Spinner],
+  imports: [PIcon, ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  icons = ['home', 'star', 'heart', 'bell'];
+
+  selectedIcon = signal<string>('home');
+}
