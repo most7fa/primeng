50170f1f84d71e1f6e206449622a0cedc93ad7da
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-28 17:00:09 +0300

    select-button

--- src/app/components/select-button/select-button.ts
+++ src/app/components/select-button/select-button.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
-  selector: 'app-select-button',
-  imports: [],
+  selector: 'app-root',
+  standalone: true,
+  imports: [SelectButtonModule, FormsModule],
   templateUrl: './select-button.html',
-  styleUrl: './select-button.css',
 })
-export class SelectButton {
+export class App {
+  stateOptions = [
+    { label: 'One-Way', value: 'one-way' },
+    { label: 'Return', value: 'return' }
+  ];
 
+  value: string = 'one-way';
 }
