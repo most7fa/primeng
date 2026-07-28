c7d9ee6cf3da19a2365860ed578587646462d4e0
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-28 17:13:35 +0300

    float-label

--- src/app/components/float-label/float-label.ts
+++ src/app/components/float-label/float-label.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  selector: 'app-float-label',
-  imports: [],
+  selector: 'app-root',
+  standalone: true,
+  imports: [FloatLabelModule, InputTextModule, FormsModule],
   templateUrl: './float-label.html',
-  styleUrl: './float-label.css',
 })
-export class FloatLabel {
-
+export class App {
+  value: string | undefined;
 }
