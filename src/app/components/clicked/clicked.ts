f1d8edce81bd083e3ca57d02d81a9953e30178d3
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-28 17:29:03 +0300

    clicked

--- src/app/components/clicked/clicked.ts
+++ src/app/components/clicked/clicked.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-clicked',
-  imports: [],
+  selector: 'app-root',
+  standalone: true,
   templateUrl: './clicked.html',
-  styleUrl: './clicked.css',
 })
-export class Clicked {
+export class App {
+
+  onButtonClick(event: MouseEvent) {
+    console.log('Clicked', event);
+    alert('Clicked');
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    console.log('Key:', event.key);
+
+    if (event.key === 'Enter' || event.key === ' ') {
+      alert('Keyboard Click');
+    }
+  }
 
 }
