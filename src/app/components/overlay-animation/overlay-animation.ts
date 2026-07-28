699a676a98013483554bf4c16133262f5d8e2f46
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-28 17:21:36 +0300

    overlay-animation

--- src/app/components/overlay-animation/overlay-animation.ts
+++ src/app/components/overlay-animation/overlay-animation.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 
 @Component({
-  selector: 'app-overlay-animation',
-  imports: [],
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, SelectModule],
   templateUrl: './overlay-animation.html',
-  styleUrl: './overlay-animation.css',
 })
-export class OverlayAnimation {
+export class App {
+  cities = [
+    { name: 'Cairo' },
+    { name: 'Alexandria' },
+    { name: 'Menofia' },
+    { name: 'Sharm Elshakh' },
+    { name: 'Mansoura' }
+  ];
 
+  selectedCity: any;
 }
