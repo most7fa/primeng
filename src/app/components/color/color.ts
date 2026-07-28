eadcd24997aa35162bd5f967c4f687c4d1efaf3d
Mostafa Gaballah <mostafa@placeholder.dev>
2026-07-28 17:35:09 +0300

    color

--- src/app/components/color/color.ts
+++ src/app/components/color/color.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Check } from '@primeicons/angular/check';
+import { Times } from '@primeicons/angular/times';
+import { Search } from '@primeicons/angular/search';
+import { User } from '@primeicons/angular/user';
 
 @Component({
-  selector: 'app-color',
-  imports: [],
+  selector: 'app-root',
+  standalone: true,
+  imports: [Check, Times, Search, User],
   templateUrl: './color.html',
-  styleUrl: './color.css',
 })
-export class Color {
-
-}
+export class App {}
